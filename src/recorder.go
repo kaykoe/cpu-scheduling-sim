3ed08eb392aeb6cb61d9422300a840cf6aed5c49
kaykoe <karolkoenigg@gmail.com>
2025-01-16 20:40:27 +0100

    added recorder.go (interface for saving simulation output)

--- src/recorder.go
+++ src/recorder.go
@@ -15,18 +15,16 @@ type Recorder interface {
 	Records() [][]string
 }
 
-// WriteRecords writes the records of the passed in Recorder to the output io.Writer encoded as csv,
+// writeRecords writes the records of the passed in Recorder to the output io.Writer encoded as csv,
 // or with tab alignment if the io.Writer is a tabwriter.Writer
-func WriteRecords(r Recorder, output io.Writer) {
+func writeRecords(r Recorder, output io.Writer) {
 	w := csv.NewWriter(output)
 	if reflect.TypeOf(output) == reflect.TypeOf(&tabwriter.Writer{}) {
 		w.Comma = '\t'
 	}
 	records := r.Records()
-	if records != nil {
-		if err := w.WriteAll(records); err != nil {
-			log.Panic(err)
-		}
+	if err := w.WriteAll(records); err != nil {
+		log.Panic(err)
 	}
 }
 
@@ -35,16 +33,19 @@ func Save(r Recorder, outDir string) {
 	if err := os.MkdirAll(outDirPath, 0775); err != nil {
 		log.Panic(err)
 	}
+	// here we name the output files with the name of the package they come from
 	rpath := reflect.Indirect(reflect.ValueOf(r)).Type().PkgPath()
 	rname := rpath[strings.LastIndexByte(rpath, '/')+1:]
 	filename := outDirPath + rname
 
+	// we write 2 files, one is  csv file for easier work with python,
+	// the other one is a txt file with tab aligned columns for readability
 	csvFile, err := os.Create(filename + ".csv")
 	defer csvFile.Close()
 	if err != nil {
 		log.Panic(err)
 	}
-	WriteRecords(r, csvFile)
+	writeRecords(r, csvFile)
 
 	textFile, err := os.Create(filename + ".txt")
 	defer textFile.Close()
@@ -52,7 +53,7 @@ func Save(r Recorder, outDir string) {
 		log.Panic(err)
 	}
 	tw := tabwriter.NewWriter(textFile, 0, 4, 3, ' ', 0)
-	WriteRecords(r, tw)
+	writeRecords(r, tw)
 	if err = tw.Flush(); err != nil {
 		log.Panic(err)
 	}
