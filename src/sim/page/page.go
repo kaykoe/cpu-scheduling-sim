8aa878600d4021582b4a7944384678f18826542a
kaykoe <karolkoenigg@gmail.com>
2025-01-17 01:25:26 +0100

    added types to represent the pages, implemented the Recorder interface, and the needed data structures, as well as the algorithms to be simulated

--- src/sim/page/page.go
+++ src/sim/page/page.go
@@ -1,13 +1,67 @@
 package page
 
 import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"math/rand/v2"
+	"os"
 	"reflect"
-	"strconv"
 )
 
 type Page struct {
-	id       uint16
-	lastUsed uint16
+	id           uint16
+	timesUsed    uint16
+	pageFaultAt  []uint16
+	swappedOutAt []uint16
+}
+
+// gen generates a random pattern of referencing a given amount of pages a given number of times
+func gen(numPages, len uint16) (referencePattern []uint16) {
+	if len == 0 {
+		log.Panic("The page reference pattern length must be greater than zero")
+	}
+	if numPages == 0 {
+		log.Panic("The number of pages to simulate must be larger than zero")
+	}
+
+	referencePattern = make([]uint16, len)
+	for i := range referencePattern {
+		referencePattern[i] = uint16(rand.UintN(uint(numPages)))
+	}
+	return referencePattern
+}
+
+// SaveReferencePattern saves the reference pattern to an output file in a .csv format
+func SaveReferencePattern(referencePattern []uint16) {
+	if referencePattern == nil {
+		log.Panic("The reference pattern slice cannot be nil")
+	}
+	if len(referencePattern) == 0 {
+		log.Panic("The reference pattern has to contain something")
+	}
+
+	record := make([]string, len(referencePattern))
+	for i, index := range referencePattern {
+		record[i] = fmt.Sprint(index)
+	}
+
+	outDirPath := "../in/"
+	if err := os.MkdirAll(outDirPath, 0775); err != nil {
+		log.Panic(err)
+	}
+	filename := outDirPath + "pageReferencePattern"
+
+	csvFile, err := os.Create(filename + ".csv")
+	defer csvFile.Close()
+	if err != nil {
+		log.Panic(err)
+	}
+	w := csv.NewWriter(csvFile)
+	if err := w.Write(record); err != nil {
+		log.Panic(err)
+	}
+	w.Flush()
 }
 
 type Slice []Page
@@ -15,58 +69,76 @@ type Slice []Page
 var pageNumFields = reflect.TypeOf(Page{}).NumField()
 
 // Records implements the Recorder interface
-func (s *Slice) Records() (records [][]string) {
-	if s != nil {
-		records = make([][]string, len(*s)+1)
-		for i := range records {
-			records[i] = make([]string, pageNumFields)
-		}
-
-		for i := range records[0] {
-			records[0][i] = reflect.TypeOf(Page{}).Field(i).Name
-		}
-
-		for i, page := range *s {
-			for j := range records[i+1] {
-				field := strconv.FormatUint(reflect.ValueOf(page).Field(j).Uint(), 10)
-				records[i+1][j] = field
-			}
-		}
-		return records
+func (pages *Slice) Records() (records [][]string) {
+	if pages == nil {
+		log.Panic("The page slice for extracting records cannot be nil")
 	}
-	return nil
+	if len(*pages) == 0 {
+		log.Panic("The page slice for extracting records cannot be empty")
+	}
+
+	records = make([][]string, len(*pages)+1)
+	for i := range records {
+		records[i] = make([]string, pageNumFields)
+	}
+
+	for i := range records[0] {
+		records[0][i] = reflect.TypeOf(Page{}).Field(i).Name
+	}
+
+	vals := records[1:]
+	for i, page := range *pages {
+		for j := range vals[i] {
+			field := fmt.Sprint(reflect.ValueOf(page).Field(j))
+			vals[i][j] = field
+		}
+	}
+	return records
 }
 
-func (s *Slice) Copy() *Slice {
-	c := make([]Page, len(*s))
-	for i := range *s {
-		c[i] = (*s)[i]
-	}
-	return (*Slice)(&c)
-}
-
+// Heap implements the container.Heap.Interface to get a page min Heap sorted by least times used (for LFU)
 type Heap []*Page
 
-func (p Heap) Len() int {
-	return len(p)
+func (h *Heap) Len() int {
+	if h == nil {
+		log.Panic("The underlying slice of a heap cannot be nil")
+	}
+
+	return len(*h)
 }
 
-func (p Heap) Less(i, j int) bool {
-	return p[i].lastUsed < p[j].lastUsed
+func (h *Heap) Less(i, j int) bool {
+	if h == nil {
+		log.Panic("The underlying slice of a heap cannot be nil")
+	}
+
+	return (*h)[i].timesUsed < (*h)[j].timesUsed
 }
 
-func (p Heap) Swap(i, j int) {
-	p[i], p[j] = p[j], p[i]
+func (h *Heap) Swap(i, j int) {
+	if h == nil {
+		log.Panic("The underlying slice of a heap cannot be nil")
+	}
+
+	(*h)[i], (*h)[j] = (*h)[j], (*h)[i]
 }
 
-func (p Heap) Push(x any) {
-	p = append(p, x.(*Page))
+func (h *Heap) Push(x any) {
+	if h == nil {
+		log.Panic("The underlying slice of a heap cannot be nil")
+	}
+
+	*h = append(*h, x.(*Page))
 }
 
-func (p Heap) Pop() any {
-	old := p
+func (h *Heap) Pop() any {
+	if h == nil {
+		log.Panic("The underlying slice of a heap cannot be nil")
+	}
+
+	old := *h
 	n := len(old)
 	x := old[n-1]
-	p = old[:n-1]
+	*h = old[:n-1]
 	return x
 }
--- src/sim/page/pagingalgs.go
+++ src/sim/page/pagingalgs.go
@@ -1,7 +1,160 @@
 package page
 
-func FIFO(s Slice) {
+import (
+	"container/heap"
+	"log"
+	"maps"
+	"math"
+	"src/sim"
+)
+
+type Alg func(referencePattern []uint16) *Slice
+
+// Sim runs a simulation of a randomly generated reference pattern using the strategies in the alg slice
+func Sim(numPages, referencePatternLen uint16, algs ...Alg) (referencePattern []uint16, res []*Slice) {
+	if numPages == 0 {
+		log.Panic("The number of pages to simulate cannot be zero")
+	}
+	if referencePatternLen == 0 {
+		log.Panic("The page reference pattern length must be greater than zero")
+	}
+	if algs == nil {
+		log.Panic("The alg slice cannot be nil")
+	}
+	if len(algs) == 0 {
+		log.Panic("The number of algorithms to simulate cannot be zero")
+	}
+
+	referencePattern = gen(numPages, referencePatternLen)
+	res = make([]*Slice, len(algs))
+	for i, alg := range algs {
+		res[i] = alg(referencePattern)
+	}
+	return referencePattern, res
 }
 
-func LFU(s Slice) {
+func FIFO(referencePattern []uint16) *Slice {
+	if referencePattern == nil {
+		log.Panic("The reference pattern slice cannot be nil")
+	}
+	if len(referencePattern) == 0 {
+		log.Panic("The reference pattern has to contain something")
+	}
+	if len(referencePattern) > math.MaxUint16 {
+		log.Panicf("The reference pattern length cannot exceed %d", math.MaxUint16)
+	}
+
+	// the page table stores whether a page is in memory
+	pageTable := make(map[uint16]bool)
+	memory := make(map[uint16]*Page)
+	swap := make(map[uint16]*Page)
+	const FRAME_SIZE = 16
+	// the delete queue stores the indices of pages that are in memory in the order they were referenced
+	// this is perfect for implementing fifo
+	deleteQueue := sim.NewQueue[uint16](FRAME_SIZE)
+
+	for _, page := range referencePattern {
+		pageTable[page] = false
+	}
+	for page := range maps.Keys(pageTable) {
+		swap[page] = &Page{
+			page,
+			0,
+			make([]uint16, 0, len(referencePattern)),
+			make([]uint16, 0, len(referencePattern))}
+	}
+
+	for i, page := range referencePattern {
+		if inMemory := pageTable[page]; !inMemory {
+			// if there is no space left in memory we need to move a page to swap
+			if len(memory) == FRAME_SIZE {
+				victimPage := deleteQueue.Pop()
+				swap[victimPage] = memory[victimPage]
+				delete(memory, victimPage)
+				swap[victimPage].swappedOutAt = append(swap[victimPage].swappedOutAt, uint16(i))
+				pageTable[victimPage] = false
+			}
+
+			memory[page] = swap[page]
+			delete(swap, page)
+			pageTable[page] = true
+			memory[page].pageFaultAt = append(memory[page].pageFaultAt, uint16(i))
+			deleteQueue.Push(page)
+			continue
+		}
+	}
+	res := Slice(make([]Page, 0, len(pageTable)))
+	for page := range maps.Values(swap) {
+		res = append(res, *page)
+	}
+	for page := range maps.Values(memory) {
+		res = append(res, *page)
+	}
+	return &res
+}
+
+func LFU(referencePattern []uint16) *Slice {
+	if referencePattern == nil {
+		log.Panic("The reference pattern slice cannot be nil")
+	}
+	if len(referencePattern) == 0 {
+		log.Panic("The reference pattern has to contain something")
+	}
+	if len(referencePattern) > math.MaxUint16 {
+		log.Panicf("The reference pattern length cannot exceed %d", math.MaxUint16)
+	}
+
+	// the page table stores whether a page is in memory
+	pageTable := make(map[uint16]bool)
+	memory := make(map[uint16]*Page)
+	swap := make(map[uint16]*Page)
+	const FRAME_SIZE = 16
+	// this heap stores pages that are in memory, sorted by the amount of times they have been used since getting swapped into memory
+	// a heap is used because heapify has better time complexity than sort, and we only need the smallest element
+	deleteHeap := new(Heap)
+	*deleteHeap = make([]*Page, 0, FRAME_SIZE)
+
+	for _, page := range referencePattern {
+		pageTable[page] = false
+	}
+	for page := range maps.Keys(pageTable) {
+		swap[page] = &Page{
+			page,
+			0,
+			make([]uint16, 0, len(referencePattern)),
+			make([]uint16, 0, len(referencePattern))}
+	}
+
+	for i, page := range referencePattern {
+		if inMemory := pageTable[page]; !inMemory {
+			// if there is no space left in memory we need to move a page to swap
+			if deleteHeap.Len() == FRAME_SIZE {
+				// here we heapify the heap so that it takes into account uses that did not require swapping
+				heap.Init(deleteHeap)
+				victimPage := heap.Pop(deleteHeap).(*Page)
+				delete(memory, victimPage.id)
+				swap[victimPage.id] = victimPage
+				victimPage.swappedOutAt = append(victimPage.swappedOutAt, uint16(i))
+				// whenever a page is swapped out it's counter is reset, so that the algorithm responds to locality changes better
+				victimPage.timesUsed = 0
+				pageTable[victimPage.id] = false
+			}
+
+			memory[page] = swap[page]
+			heap.Push(deleteHeap, memory[page])
+			delete(swap, page)
+			pageTable[page] = true
+			memory[page].pageFaultAt = append(memory[page].pageFaultAt, uint16(i))
+		}
+		// if the page was already in memory we just increment the amount of times it was used
+		memory[page].timesUsed++
+	}
+	res := Slice(make([]Page, 0, len(pageTable)))
+	for page := range maps.Values(swap) {
+		res = append(res, *page)
+	}
+	for page := range maps.Values(memory) {
+		res = append(res, *page)
+	}
+	return &res
 }
